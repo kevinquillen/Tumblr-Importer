3dc9eb40aa72230972bb41c49db9acbef40aba17
kevinquillen <kevin@inclind.com>
2012-11-01 12:08:47 -0400

    Fix up the OAuth code, implement an oauth callback. That URL must be the same in the app and this script. Ask for the oauth verifier to get the access token. Pass parameters to tumblr_push and send them up via the API. Ask for the users blog hostname at the start so we know what account to post to.

--- converter.rb
+++ converter.rb
@@ -4,6 +4,9 @@ require 'nokogiri'
 require 'date'
 require 'oauth'
 
+puts "What is your Tumblr blog hostname (example: myblog.tumblr.com)? Hostname: "
+blog_hostname = gets.strip
+
 puts "What is your Tumblr OAuth Consumer Key? This is used to establish OAuth connection. Consumer Key: "
 consumer_key = gets.strip
 
@@ -11,14 +14,14 @@ puts "What is your Tumblr OAuth Consumer Secret Key? This is used to establish O
 consumer_secret = gets.strip
 
 class TumblrImport
-  attr_accessor :consumer_key, :consumer_secret, :request_token
+  attr_accessor :blog_hostname, :consumer_key, :consumer_secret, :request_token, :access_token
 
-  def initialize (consumer_key, consumer_secret)
+  def initialize (blog_hostname, consumer_key, consumer_secret)
+    @blog_hostname = blog_hostname
     @consumer_key = consumer_key
     @consumer_secret = consumer_secret
     puts "Authenticating with Tumblr..."
-    oauth_request_token
-    # if request token is bad or rejected, stop here
+    oauth_authorize
     puts "Searching for .html posts to send to Tumblr..."
     recurse_directories
   end
@@ -38,38 +41,48 @@ class TumblrImport
 
   def parse_file file
     post = Nokogiri::HTML(open(file))
-    title = post.css('h1.entry-title').text
-    body = post.css('div.entry-content').text
+    title = post.css('h1.entry-title').text.to_s
+    body = post.css('div.entry-content').text.to_s
     tags = post.css('span.categories a').map{|category| category.content}.join(', ')
     published = post.css('time')[0]['datetime']
     date = DateTime.parse(published)
-    formatted_date = date.strftime('%Y-%m-%d %I:%M:%S GMT')
+    formatted_date = date.strftime('%Y-%m-%d %I:%M:%S GMT').to_s
+
+    params = {
+      :type => 'text',
+      :title => title.to_s,
+      :body => body.to_s,
+      :tags => tags,
+      :date => formatted_date.to_s
+    }
 
     puts "Pushing \"#{title}\" published on #{published} up to Tumblr.."
-    tumblr_push title, body, tags, published
+    tumblr_push params
   end
 
-  def tumblr_push title, body, tags, published
-    type = 'text'
-    # this will push our arguments as a Tumblr post via /post of Tumblr API
-    # waiting for x_auth approval, can't go any further yet.
+  def tumblr_push params
+    @access_token.post("http://api.tumblr.com/v2/blog/#@blog_hostname/post", params)
 
-    # throttle the request so we don't overwhelm Tumblr API by hammering it with a large set of files
-    sleep(3)
+    # throttle the request so we don't overwhelm Tumblr API by hammering it with a large import of files
+    sleep(2)
   end
 
-  def oauth_request_token
+  def oauth_authorize
     @consumer = OAuth::Consumer.new(@consumer_key, @consumer_secret,
-                                  :site => 'http://www.tumblr.com/',
+                                  :site => 'http://www.tumblr.com',
                                   :scheme => :header,
                                   :method => :post,
+                                  :oauth_callback => 'http://localhost:8082/',
                                   :request_token_path => '/oauth/request_token',
                                   :access_token_path => '/oauth/access_token',
                                   :authorize_path => '/oauth/authorize')
 
     @request_token = @consumer.get_request_token
+    puts "Please go here and authorize access to your account: #{@request_token.authorize_url}"
+    puts "If you granted access, please enter the OAuth Verifier token from the URL:"
+    @access_token = @request_token.get_access_token({:oauth_verifier => gets.strip})
   end
 end
 
 # Start import
-TumblrImport.new(consumer_key, consumer_secret)
+TumblrImport.new(blog_hostname, consumer_key, consumer_secret)
